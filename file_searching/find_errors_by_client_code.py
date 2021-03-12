e412156e1d76de9f5d4cc92eed55c1a36a1040b9
Tamilyn Peck <65209760+tamilynpeck@users.noreply.github.com>
2021-03-12 14:09:21 -0700

    file searching code in progress

--- file_searching/find_errors_by_client_code.py
+++ file_searching/find_errors_by_client_code.py
@@ -6,7 +6,7 @@ from datetime import date
 from datetime import datetime
 import pandas as pd
 
-ERROR_CODE = "ExistingDependentSentInPrimaryPositionCannotHaveDependent"
+ERROR_CODE = "SqlException"
 
 
 def skip_if_not_expected_file_type(path, file_type=".csv"):
@@ -16,27 +16,26 @@ def skip_if_not_expected_file_type(path, file_type=".csv"):
         return False
 
 
-def review_file(file, file_path, df):
-    # try:
-    file_df = pd.read_csv(file_path, header=None)
-    columns = [file_df.columns[1]]
-    columns.extend(file_df.columns[-2:])
-    file_df = file_df[columns].copy()
-    file_df.columns = ["FileName", "ErrorMessage", "ErrorCode"]
-    file_df["FileName"] = file
-    file_df = file_df[file_df["ErrorCode"].str.contains(ERROR_CODE, na=False)]
-    # file_df = file_df[file_df["ErrorCode"] == ERROR_CODE]
+def review_file(file_name, file_path, df):
+    try:
+        file_df = pd.read_csv(file_path, header=None)
+        columns = [file_df.columns[1]]
+        columns.extend(file_df.columns[-2:])
+        file_df = file_df[columns].copy()
+        file_df.columns = ["FileName", "ErrorMessage", "ErrorCode"]
+        file_df = file_df[file_df["ErrorCode"].str.contains(ERROR_CODE)]
+        # file_df = file_df[file_df["ErrorCode"] == ERROR_CODE]
 
-    file_df.drop_duplicates(inplace=True)
-    file_df.dropna(how="all", inplace=True)
-    df = df.append(file_df)
-    df["ErrorMessage"] = df["ErrorMessage"].str.replace(r"\d+", "", regex=True)
-    df.drop_duplicates(inplace=True)
+        file_df.drop_duplicates(inplace=True)
+        file_df.dropna(how="all", inplace=True)
+        df = df.append(file_df)
+        df["ErrorMessage"] = df["ErrorMessage"].str.replace(r"\d+", "", regex=True)
+        df.drop_duplicates(inplace=True)
 
-    return df
+        return df
 
-    # except Exception as ex:
-    #     print(f"{type(ex)} Error with file: {file}")
+    except Exception as ex:
+        print(f"{type(ex)} Error with file: {file_name}")
 
 
 def loop_thru_folder(
@@ -44,7 +43,7 @@ def loop_thru_folder(
     earliest_file_date,
     latest_file_date=str(datetime.today()),
     maximum_len=100,
-    # ignore_file = 0
+    limit_client_code=None,
     # skip_analyst_fix = False
     # skip_analyst_change = False
     # skip_campaign_fix = False
@@ -55,10 +54,13 @@ def loop_thru_folder(
     for path, subdir, files in os.walk(folder_location):
         print(f"Reviewing Folder... {subdir}")
 
-        for file in files:
+        for file_name in files:
             if len(df) > maximum_len:
                 return df
-            file_path = os.path.join(path, file)
+            file_path = os.path.join(path, file_name)
+
+            if limit_client_code not in file_name:
+                continue
 
             if skip_if_not_expected_file_type(file_path, ".csv"):
                 continue
@@ -82,8 +84,8 @@ def loop_thru_folder(
             if file_size == 0:
                 continue
 
-            print(f"Reading {file}, Size {file_size}")
-            df = review_file(file, file_path, df)
+            print(f"Reading {file_name}, Size {file_size}")
+            df = review_file(file_name, file_path, df)
             print(f"Number Of Errors Found: {len(df)}")
 
     return df
@@ -99,16 +101,19 @@ def export_file(df, name, index=False):
 
 def framework():
     folder_location = (
-        "\\\\prodjobdata.extendhealth.com\\EligibilityFileImportNotImported\\2021\\1"
+        "\\\\prodjobdata.extendhealth.com\\EligibilityFileImportNotImported\\2021"
     )
-    # folder_location = r'\\\\secureshare\\Encrypted Share\\Delivery Management\\Eligibility\\ErrorOutputs\\EtlErrors'
-    earliest_file_date = "2021-01-01"
+    earliest_file_date = "2020-01-01"
     latest_file_date = str(datetime.today())
+    limit_client_code = "AMTR"
 
     df = loop_thru_folder(
-        folder_location, earliest_file_date, latest_file_date, maximum_len=10
+        folder_location=folder_location,
+        earliest_file_date=earliest_file_date,
+        latest_file_date=latest_file_date,
+        limit_client_code=limit_client_code,
     )
-    export_file(df, ERROR_CODE)
+    export_file(df, f"SqlException_{limit_client_code}")
 
 
 if __name__ == "__main__":
